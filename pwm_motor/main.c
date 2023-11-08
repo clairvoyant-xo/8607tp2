a75df15b1d845d2e8e5f3f73de5fa5aae9fdace5
Martin <73581070+clairvoyant-xo@users.noreply.github.com>
2023-11-07 21:01:56 -0300

    Add files via upload

--- pwm_motor/main.c
+++ pwm_motor/main.c
@@ -7,19 +7,20 @@
 volatile uint8_t velocidad;
 volatile uint8_t sentido;
 
-const uint8_t vel[3] = {0,210,255};
+const uint8_t vel_m3[5] = {0,136,141,147,149};
+const uint8_t vel_m4[5] = {0,136,141,147,149};
 const uint8_t sent[3] = {0x00,0x84,0x03};
 
 ISR(PCINT2_vect){
     uint8_t estado = PIND;
 
     if((estado & 0x01) == 0x00){
-        if(velocidad == 2){
+        if(velocidad == 4){
         return;
         }
 
-        OCR0A = vel[++velocidad];
-        OCR0B = vel[velocidad];
+        OCR0A = vel_m3[++velocidad];
+        OCR0B = vel_m4[velocidad];
         _delay_ms(DELAY);        
         return;
     }
@@ -29,8 +30,8 @@ ISR(PCINT2_vect){
         return;
         }
 
-        OCR0A = vel[--velocidad];
-        OCR0B = vel[velocidad];
+        OCR0A = vel_m3[--velocidad];
+        OCR0B = vel_m4[velocidad];
         _delay_ms(DELAY);        
         return;
     }
@@ -46,8 +47,8 @@ void init_timer(void){
     TCCR0A = (1 << COM0A1) | (1 << COM0B1) | (1 << WGM01) | (1 << WGM00);
     TCCR0B = (1 << CS00);
     TCNT0 = 0; 
-    OCR0A = vel[velocidad];
-    OCR0B = vel[velocidad];
+    OCR0A = vel_m3[velocidad];
+    OCR0B = vel_m4[velocidad];
 }
 
 void init_shield(void){
